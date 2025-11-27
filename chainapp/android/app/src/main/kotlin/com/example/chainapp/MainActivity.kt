e87eeede66c537c0594ad8a876aea5008895fbd9
Hanife <h4nifekorkmaz@gmail.com>
2025-11-27 21:54:27 +0300

    Initial commit: Flutter project setup and Firebase integration

---
+++ chainapp/android/app/src/main/kotlin/com/example/chainapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chainapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ chainapp/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // firebase_core'u eklediğinizden emin olun
+
+// flutterfire configure ile oluşturulan ve bağlantı bilgilerini içeren dosya
+import 'firebase_options.dart';
+
+void main() async {
+  // Flutter motorunun (binding) başlatılmasını bekle
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase'i başlat
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+//H: öncesini silme otomatik olarak firebase i̇nitialize ediyor
