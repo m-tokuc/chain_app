b9208b65a23abbdecd2bea56bbfe7849b923b7cb
Hanife <h4nifekorkmaz@gmail.com>
2025-12-01 14:56:56 +0300

    normal

--- chainapp/lib/main.dart
+++ chainapp/lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart'; // firebase_core'u eklediğinizden emin olun
-
-// flutterfire configure ile oluşturulan ve bağlantı bilgilerini içeren dosya
-import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-  // Flutter motorunun (binding) başlatılmasını bekle
+  // Firebase kullanacaksan bu iki satır main içinde şarttır:
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Firebase'i başlat
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
 }
-//H: öncesini silme otomatik olarak firebase i̇nitialize ediyor
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chain App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(body: Center(child: Text('Merhaba Chain App!'))),
+    );
+  }
+}
